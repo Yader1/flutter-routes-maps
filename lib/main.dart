03d023730c74daad48843996bf928d0a8f1d0883
Yader Campbell <yadermejiac123@gmail.com>
2024-11-14 22:02:24 -0600

    feat: Screens and directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import '/screens/screens.dart';
+
+void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'MapsApp',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home: const LoadingScreen()
     );
   }
 }
