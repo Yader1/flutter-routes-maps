54db683d1b1f39f28919349bf8ad7317e3428c9f
Yader Oneall Campbell <yadermejiac123@gmail.com>
2025-04-09 21:17:57 -0600

    feat: Create pad to control the map

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ void main() {
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(create: (context) => GpsBloc()),
-      BlocProvider(create: (context) => LocationBloc())
+      BlocProvider(create: (context) => LocationBloc()),
+      BlocProvider(create: (context) => MapBloc()),
     ],
     child: const MyApp()
   ));
