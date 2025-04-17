9d6941a3f0f13d7c4b378427b61cd886cd2dee7f
Yader Oneall Campbell <yadermejiac123@gmail.com>
2025-04-17 14:26:57 -0600

    feat: Changing the map style

--- lib/blocs/map/map_bloc.dart
+++ lib/blocs/map/map_bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../themes/themes.dart';
 
 part 'map_event.dart';
 part 'map_state.dart';
@@ -14,6 +18,7 @@ class MapBloc extends Bloc<MapEvent, MapState> {
 
   void _onInitMap(OnMapInitialzedEvent event, Emitter<MapState> emit) {
     _mapController = event.controller;
+    _mapController!.setMapStyle(jsonEncode(uberMapTheme));
 
     emit(state.copyWith(isMapInitialized: true));
   }
