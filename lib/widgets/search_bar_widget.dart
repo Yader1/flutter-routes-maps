0cbbead2a5fa47c69c6a0ed3e8cbef72ba1b7c6f
Yader Campbell <yadermejiac123@gmail.com>
2025-08-21 21:52:23 -0600

    feat: Show and hide UI elements based on manual search

--- lib/widgets/search_bar_widget.dart
+++ lib/widgets/search_bar_widget.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../blocs/blocs.dart';
 import '../delegates/delegates.dart';
+import '../models/models.dart';
 
 class SearchBarWidget extends StatelessWidget {
   const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SearchBloc, SearchState>(
+      builder: (context, state){
+        return state.displayManualMarker
+          ? const SizedBox()
+          : const _SearchBarBodyWidget();
+      }
+    );
+  }
+}
+
+class _SearchBarBodyWidget extends StatelessWidget {
+  const _SearchBarBodyWidget();
+
+  void onSearchResults(BuildContext context, SearchResult result) {
+    final searchBloc = BlocProvider.of<SearchBloc>(context);
+    if(result.manual == true){
+      searchBloc.add(OnActivateManualMarkerEvent());
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +42,8 @@ class SearchBarWidget extends StatelessWidget {
           onTap: () async {
             var result = await showSearch(context: context, delegate: SearchDestinationDelegate());
             if(result == null) return;
+
+            onSearchResults(context, result);
           },
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 13),
