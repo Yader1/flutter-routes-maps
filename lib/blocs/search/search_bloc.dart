103cb00249ea3fd385a9fdf282b1b1eb84280b9a
Yader Campbell <yadermejiac123@gmail.com>
2025-10-28 22:59:10 -0700

    feat: Decode geometry

--- lib/blocs/search/search_bloc.dart
+++ lib/blocs/search/search_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 
+import '../../models/route_destination.dart';
 import '../../services/services.dart';
 
 part 'search_event.dart';
@@ -18,8 +20,21 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     on<OnActivateManualMarkerEvent>((event, emit) => emit(state.copyWith(displayManualMarker: false)));
   }
 
-  Future getCoorsStartToEnd(LatLng start, LatLng end) async {
-    final resp = await trafficService.getCoorsStartToEnd(start, end);
-    return resp;
+  Future<RouteDestination> getCoorsStartToEnd(LatLng start, LatLng end) async {
+    final trafficResponse = await trafficService.getCoorsStartToEnd(start, end);
+
+    final geometry = trafficResponse.routes[0].geometry;
+    final distance = trafficResponse.routes[0].distance;
+    final duration = trafficResponse.routes[0].duration;
+
+    final points = decodePolyline(geometry, accuracyExponent: 6)
+      .map((coor) => LatLng(coor[0].toDouble(), coor[1].toDouble()))
+      .toList();
+    
+    return RouteDestination(
+      points: points, 
+      duration: duration, 
+      distance: distance
+    );
   }
 }
